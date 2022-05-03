ae7f4286b776aa3ab24843a9c41e37bdf60e05de
Alexey Piltenko <ukr.top@ukr.net>
2022-05-03 21:12:37 +0300

    1 punkt

--- README..md
+++ README..md
@@ -1,1 +1,11 @@
-Text
+# Node.js random03
+
+When a user enters node index.js in the command line, a block of approximately 31x9 # characters colored with a random color (using hex code, e.g. #ff0000) should be generated.
+
+import chalk from 'chalk';
+
+console.log(chalk.red('##########'));
+const
+randomColor();
+
+const messablock
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('test');
+import chalk from 'chalk';
+import randomColor from 'randomColor';
+
+const color = randomColor();
+randomColor();
+
+const messageblock = `  ###############################
+  ###############################
+  ###############################
+  ###############################
+  ####                       ####
+  ####        ${color}        ####
+  ####                       ####
+  ###############################
+  ###############################
+  ###############################
+  ###############################`;
+
+console.log(chalk.hex(`${color}`).bold(`${messageblock}`));
